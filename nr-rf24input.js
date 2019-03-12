cfb07991004dd73f6ef8b7aee009edd2f072b740
ludiazv <ldiaz@atloideas.com>
2019-03-12 15:36:43 +0100

    0.1.0 pre release

--- nr-rf24input.js
+++ nr-rf24input.js
@@ -1,51 +1,72 @@
-'use strict';
+"use strict";
 
 module.exports = function(RED) {
-  function RF24input(n) {
-    RED.nodes.createNode(this,n);
-    var node = this;
-    //var context=this.context();
-
-    node.pipeID=-1;
-    node.pipeAddress=n.pipeaddress;
-    node.autoAck=n.autoack;
-    node.topic= n.topic || "";
-    node.as_string=n.outputstring || false;
-    node.radio=RED.nodes.getNode(n.radio);
-    node.rxPck=0;
-
-    // Rcv Handler
-    node.rcv=function(data){
-      var msg={_msgid:RED.util.generateId(),
-               pipeID:node.pipeID,
-               pipeAddress: node.pipeAddress,
-               topic: node.topic,
-               payload: (node.as_string) ? node.radio.convertToString(data) : data};
-      node.send(msg);
-      node.rxPck++;
-      node.status({fill:"green",shape:"dot",text:"A:"+ node.pipeAddress + " /Rx:"+node.rxPck});
-    };
-
-    if(node.radio.radio_ok && !node.radio.is_locked()) {
-      node.radio.use();
-      node.pipeID=node.radio.registerReader(node.pipeAddress,node.autoAck,node.rcv);
-      if(node.pipeID>=1){
-        node.on('close',function(remove,done){
-          node.radio.deregisterReader(node.pipeID);
-          node.radio.release();
-          done();
-        });
-        this.status({fill:"green",shape:"dot",text:"A:"+ node.pipeAddress});
-      }
-      else{
+  
+    function RF24input(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+        
         node.pipeID=-1;
-        this.status({fill:"red",shape:"ring",text:"Could not open Reading pipe " + node.pipeAddress});
-        node.radio.release();
-      }
-    } else
-        this.status({fill:"red",shape:"ring",text:"RF24 not working or in use"});
+        node.pipeAddress=n.pipeaddress;
+        node.autoAck=n.autoack;
+        node.topic= n.topic || "nrf24";
+        node.as_string=n.outputstring || false;
+        node.radio=RED.nodes.getNode(n.radio);
+        node.mergeframes= n.mergeframes || 1;
+        node.mergetimeout= n.mergetimeout || 1000;
+      
+        // Rcv Handler
+        var mergeBuff=Buffer.alloc(0);
+        const mergeSize= node.mergeframes * node.radio.nrf24_configuration.PayloadSize;
+        var timeoutControl=null;
+        var emitter=function() {
+            if(timeoutControl!=null) {
+                clearTimeout(timeoutControl); // clear the timeout
+                timeoutControl=null;
+            }
+            if(mergeBuff.length>0) {
+                var data=Buffer.from(mergeBuff); // copy the buffer
+                mergeBuff=Buffer.alloc(0); // reset mergeBuff
+                var msg={_msgid:RED.util.generateId(),
+                    pipeID:node.pipeID,
+                    pipeAddress: node.pipeAddress,
+                    topic: node.topic,
+                    payload: (node.as_string) ? node.radio.convertToString(data) : data
+                };
+                node.send(msg);
+            }
+        };
+        // Receive callaback
+        node.rcv=function(data,rxStat){
+            if(timeoutControl==null && n.mergeframes> 1) { // If no timeout set programm itº
+                timeoutControl=setTimeout(emitter,node.mergetimeout);
+            }
+            mergeBuff=Buffer.concat([mergeBuff,data],mergeBuff.length+data.length);
+            if(mergeBuff.length >= mergeSize || n.mergeframes==1) emitter();
+            node.status({fill:"green",shape:"dot",text:"A:"+ node.pipeAddress + " /Rx:"+rxStat});
+        };
 
-  } // RF24input
+        if(node.radio.radio_ok && !node.radio.is_locked()) {
+            node.radio.use();
+            node.pipeID=node.radio.registerReader(node.pipeAddress,node.autoAck,node.rcv,node.mergeframes);
+            if(node.pipeID>=1){
+                node.on("close",function(remove,done){
+                    node.radio.deregisterReader(node.pipeID);
+                    node.radio.release();
+                    node.log("input node stopped");
+                    done();
+                });
+                this.status({fill:"green",shape:"dot",text:"A:"+ node.pipeAddress});
+            }
+            else{
+                node.pipeID=-1;
+                this.status({fill:"red",shape:"ring",text:"Could not open Reading pipe " + node.pipeAddress});
+                node.radio.release();
+            }
+        } else
+            this.status({fill:"red",shape:"ring",text:"RF24 not working or in use"});
 
-  RED.nodes.registerType("RF24input",RF24input);
+    } // RF24input
+
+    RED.nodes.registerType("RF24input",RF24input);
 };
--- nr-rf24output.js
+++ nr-rf24output.js
@@ -1,41 +1,104 @@
+"use strict";
+
 module.exports = function(RED) {
-  function RF24output(n) {
-    RED.nodes.createNode(this,n);
-    var node = this;
+  
+    function RF24output(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
 
-    node.pipeAddress=n.pipeaddress;
-    node.radio=RED.nodes.getNode(n.radio);
-    node.txPck=0;
-    node.txFail=0;
+        node.pipeAddress= "" + n.pipeaddress;
+        node.radio=RED.nodes.getNode(n.radio);
+        node.topic= n.topic || "";
+        node.autoack= (n.autoack === undefined) ? true : n.autoack;
+        node.writemode= parseInt(n.writemode) || 0;
+        node.streamsize= parseInt(n.streamsize) || 512;
+        node.txPck=0;
+        node.txFail=0;
 
-    if(node.radio.radio_ok && !node.radio.is_locked()) {
-      node.radio.use();
-      node.on('input',function(msg) {
-        var addr=("pipeAddressW" in msg) ? msg.pipeAddressW : node.pipeAddress;
-        if(node.radio.write(node.radio.convertToBuffer(msg.payload),addr)) {
-          node.txPck++;
-        } else {
-          node.txFail++;
-          node.warn("RF24 could not send radio packet in " + addr);
+        // Update node status symbol
+        var update_status=function(addrs){
+            let totalpck=node.txFail+node.txPck;
+            let txf_ratio=node.txFail/totalpck;
+            let color="green";
+            let shape="dot";
+            if(txf_ratio >0.75) {
+                color="red";
+                shape="ring";
+            } else if(txf_ratio > 0.25) { shape="ring"; color="yellow";}
+            node.status({fill:color,shape:shape,text:"A:"+ addrs + 
+                         "/N:" + totalpck +
+                         "/Tx:"+node.txPck +"(" + (txf_ratio*100.0).toFixed(1) +"%)"});
+        };
+        var get_async_wcallback= function(addr,msg,ack) {
+            if(ack)
+                return function(success,tx_ok,tx_b,req){                  
+                        msg.rf24_ack=success;
+                        node.send(msg); // propagate ack;
+                        node.txPck+=tx_ok;
+                        node.txFail+=req-tx_ok;
+                        update_status(addr);};
+            else return function(success,tx_ok,tx_b,req){
+                         node.txPck+=tx_ok;
+                         node.txFail+=req-tx_ok;
+                         update_status(addr);};
         }
-        var txf_ratio=node.txFail/(node.txFail+node.txPck);
-        var color="green";
-        var shape="dot";
-        if(txf_ratio >0.75) {
-           color="red";
-           shape="ring";
-        }
-        else if(txf_ratio > 0.25) color="orange";
-        node.status({fill:color,shape:shape,text:"A:"+ addr + "/ Tx:"+node.txPck +"/ Fr%=" + txf_ratio.toFixed(1)*100 +"%"});
-      });
-      node.on('close',function(remove,done){ // Destructor
-        node.radio.release();
-        done();
-      });
+        // Init
+        if(node.radio.radio_ok && !node.radio.is_locked()) {
+            node.radio.use();
+            node.on("input",function(msg) {
+                // Manage inputs
+                var addr=("pipeAddressW" in msg) ? msg.pipeAddressW : node.pipeAddress;
+                var aAck=("pipeAckW" in msg) ? msg.pipeAck : node.autoack;
+                aAck= (aAck) ? true: false; // force boolean
+                var buffer=node.radio.convertToBuffer(msg.payload);
+                if(buffer === null || buffer === undefined) {
+                    node.error("Invalid payload to send over the radio");
+                    return null;
+                }
+                // Write process
+                if(aAck) msg.rf24_ack=false; // Default false as response
+                switch(node.writemode) {
+                    case 0: //sync write
+                        let success;
+                        if((success=node.radio.write(buffer,addr,aAck))) {
+                            node.txPck++;
+                        } else{
+                            node.txFail++;
+                        }
+                        if(aAck) msg.rf24_ack=success; // Update messge
+                        update_status(addr);  // show status
+                        if(aAck) return msg;  // send sync to node-red
+                        break;
+                    case 1: // Async write
+                    case 2: // stream;
+                        let callback=get_async_wcallback(addr,msg,aAck);
+                        let job_success=node.radio.write(node.writemode,buffer,addr,aAck,callback,node.streamsize);
+                      
+                        //if(!job_success) {
+                        //    node.txFail++;
+                        //    node.error("Async job could not be started.");
+                        //}
+                        break;
+                    default:
+                        node.error("Invalid writemode ->"+node.writemode);
+                } // Switch
+      
+                return null;
 
-      node.status({fill:"green",shape:"dot",text:"A:"+ node.pipeAddress});
-    } else node.status({fill:"red",shape:"ring",text:"RF24 not working or in use"});
+            });
+            // Relase node
+            node.on("close",function(remove,done){ // Destructor
+                node.log("output node stopped removed:" + remove);
+                node.radio.release();
+                done();
+            });
+            var mode="async write";
+            if(node.writemode==0) mode="sync write";
+            if(mode.writemode==2) mode="async stream";
+            node.log("Output Pipe address configured " + node.pipeAddress+"["+mode+"]");
+            node.status({fill:"green",shape:"dot",text:"A:"+ node.pipeAddress});
+        } else node.status({fill:"red",shape:"ring",text:"RF24 not working or in use"});
 
-  }
-  RED.nodes.registerType("RF24output",RF24output);
+    }
+    RED.nodes.registerType("RF24output",RF24output);
 };
