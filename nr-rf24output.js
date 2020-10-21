bc546523247c219d1121caa196bc53355248b168
L Diaz <ldiaz@navarone.local>
2020-10-21 11:56:57 +0200

    Documentation on node + refactor write to promises.

--- nr-rf24output.js
+++ nr-rf24output.js
@@ -1,7 +1,9 @@
 "use strict";
 
 module.exports = function(RED) {
-  
+    
+    const nrutil=require("./nr-util");
+
     function RF24output(n) {
         RED.nodes.createNode(this,n);
         var node = this;
@@ -16,8 +18,8 @@ module.exports = function(RED) {
         node.txPck=0;
         node.txFail=0;
 
-        // Update node status symbol
-        var update_status=function(addrs){
+        // Update node status symbol helper
+        const update_status=function(addrs){
             let totalpck=node.txFail+node.txPck;
             let txf_ratio=node.txFail/totalpck;
             let color="green";
@@ -29,61 +31,35 @@ module.exports = function(RED) {
             var stats=(node.hidestats) ? "" : "/N:" + totalpck + "|Tx:"+node.txPck +"|" + (txf_ratio*100.0).toFixed(0) +"%";
             node.status({fill:color,shape:shape,text:"A:"+ addrs + stats});
         };
-        var get_async_wcallback= function(addr,msg,ack) {
-            if(ack)
-                return function(success,tx_ok,tx_b,req){                  
-                        msg.rf24_ack=success;
-                        node.send(msg); // propagate ack;
-                        node.txPck+=tx_ok;
-                        node.txFail+=req-tx_ok;
-                        update_status(addr);};
-            else return function(success,tx_ok,tx_b,req){
-                         node.txPck+=tx_ok;
-                         node.txFail+=req-tx_ok;
-                         update_status(addr);};
-        }
+  
         // Init
         if(node.radio.radio_ok && !node.radio.is_locked()) {
             node.radio.use();
-            node.on("input",function(msg) {
+            node.on("input",function(msg,send,done) {
+                // pre 1.0 compatibility
+                send = send || function() { node.send.apply(node,arguments) }
                 // Manage inputs
                 var addr=("pipeAddressW" in msg) ? msg.pipeAddressW : node.pipeAddress;
                 var aAck=("pipeAckW" in msg) ? msg.pipeAck : node.autoack;
                 aAck= (aAck) ? true: false; // force boolean
-                var buffer=node.radio.convertToBuffer(msg.payload);
+                var buffer=nrutil.convertToBuffer(msg.payload);
                 if(buffer === null || buffer === undefined) {
                     node.error("Invalid payload to send over the radio");
                     return null;
                 }
                 // Write process
-                if(aAck) msg.rf24_ack=false; // Default false as response
-                switch(node.writemode) {
-                    case 0: //sync write
-                        let success;
-                        if((success=node.radio.writeSync(buffer,addr,aAck))) {
-                            node.txPck++;
-                        } else{
-                            node.txFail++;
-                        }
-                        if(aAck) msg.rf24_ack=success; // Update messge
-                        update_status(addr);  // show status
-                        if(aAck) node.send(msg);  // send sync to node-red
-                        break;
-                    case 1: // Async write
-                    case 2: // stream;
-                        let callback=get_async_wcallback(addr,msg,aAck);
-                        node.radio.write(node.writemode,buffer,addr,aAck,callback,node.streamsize);
-                        //if(!job_success) {
-                        //    node.txFail++;
-                        //    node.error("Async job could not be started.");
-                        //}
-                        break;
-                    default:
-                        node.error("Invalid writemode ->"+node.writemode);
-                } // Switch
-      
-                return null;
+                node.radio.write(node.mode,buffer,addr,aAck,node.streamsize).then( function(resp) {
+                  msg.rf24_ack=resp.success;
+                  if(node.topic!="") msg.topic=node.topic; // change topic if received
+                  send(msg,false); // Propagate message without clonging
+                  if(done) done(); // Notificate done in >1.0
+                  node.txPck+=resp.tx_ok;
+                  node.txFail+=resp.req-resp.tx_ok;
+                  update_status(addr);  
 
+                }).catch(s => node.warn(s) );
+                return null;      
+                
             });
             // Relase node
             node.on("close",function(remove,done){ // Destructor
@@ -92,8 +68,8 @@ module.exports = function(RED) {
                 done();
             });
             var mode="async write";
-            if(node.writemode==0) mode="sync write";
-            if(mode.writemode==2) mode="async stream";
+            if(node.writemode==node.radio.WRITE_SYNC) mode="sync write";
+            if(mode.writemode==node.radio.WRITE_STREAM) mode="async stream";
             node.log("Output Pipe address configured " + node.pipeAddress+"["+mode+"]");
             node.status({fill:"green",shape:"dot",text:"A:"+ node.pipeAddress});
         } else node.status({fill:"red",shape:"ring",text:"RF24 not working or in use"});
